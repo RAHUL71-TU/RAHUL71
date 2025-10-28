59064be47cdf1823cc2c1627eb07d8dfb6ddc2b8
RAHUL71-TU <meb25071@tezu.ac.in>
2025-10-28 14:41:11 +0530

    Create Introduction.c

---
+++ LAB1/Introduction.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+    // Printing personal details
+    printf("----- My Introduction -----\n");
+    printf("Name     : Rahul Chetry\n");
+    printf("Dept   : Mechanical Engineering\n");
+    printf("Roll No  : MEB25071\n");
+    printf("INSTITUTE   : TEZPUR UNIVERSITY\n");
+    printf("Hobbies  : Sketching,Online games, Playing Cricket\n");
+
+    return 0;
+}
